0430408783cbf582b7b2908945af8af115ab52d2
Flying-Pegasus <atulya1804@gmail.com>
2024-09-27 18:58:04 +0530

    Adding SignUpLogin

--- prep_a_thon/src/App.js
+++ prep_a_thon/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import LoginPage from './components/LoginPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/LoginSignUp/Home';
+import LoginPage from './pages/LoginSignUp/LoginPage';
+import Signup from './pages/LoginSignUp/Signup';
 
 function App() {
   return (
-    <LoginPage/>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/new" element={<Home />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
